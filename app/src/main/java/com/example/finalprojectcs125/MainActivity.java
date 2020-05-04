78accdfc95a471c7f392018b98f5444f6f4d613b
qasim <kassimand@gmail.com>
2020-05-03 23:19:34 -0500

    Merge branch 'master' of C:\Users\qasim\AndroidStudioProjects\Final_Project_CS125 with conflicts.

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.toolbox.StringRequest;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
@@ -41,11 +43,20 @@ public class MainActivity extends AppCompatActivity {
     public void openFlightActivity() {
         Intent intent = new Intent(this, flight.class);
         startActivity(intent);
+
     }
 
     public void openVirusActivity() {
         Intent intent = new Intent(this, virus.class);
         startActivity(intent);
+        queue.cancelAll(TAG_SEARCH_NAME);
+
+        // first StringRequest: getting items searched
+        StringRequest stringRequest = searchNameStringRequest(txtSearch.getText().toString());
+        stringRequest.setTag(TAG_SEARCH_NAME);
+
+        // executing the request (adding to queue)
+        queue.add(stringRequest);
     }
 
 
