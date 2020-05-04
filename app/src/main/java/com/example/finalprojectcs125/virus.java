4c9bba01856e5e31c1035f5f74d5b6c81c72a60e
abbask1 <aak9@illinois.edu>
2020-05-04 09:16:18 +0500

    new code

--- app/src/main/java/com/example/finalprojectcs125/virus.java
+++ app/src/main/java/com/example/finalprojectcs125/virus.java
@@ -3,12 +3,97 @@ package com.example.finalprojectcs125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class virus extends AppCompatActivity {
+
+    Button button2;
+    TextView virus_data;
+    RequestQueue requestQueue;
+    String url = "https://api.covid19api.com/live/country/united-states";
+
+    private void setText(String province, int conf, int deaths, int active) {
+        this.virus_data.setText(province);
+        this.virus_data.setText(conf);
+        this.virus_data.setText(deaths);
+        this.virus_data.setText(active);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_virus);
+        this.button2 = (Button) findViewById(R.id.button2);  // Link to button.
+        this.virus_data = (TextView) findViewById(R.id.virus_data);  // Link to text output box.
+        this.virus_data.setMovementMethod(new ScrollingMovementMethod());
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String state = jsonObj.get("Province").toString();
+                                    int confirmed = jsonObj.getInt("Confirmed");
+                                    int deaths = jsonObj.getInt("Deaths");
+                                    int active = jsonObj.getInt("Active");
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+
+                            }
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        setText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
+
     }
+
+
+
+
 }
