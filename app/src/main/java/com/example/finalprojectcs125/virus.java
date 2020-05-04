2e1cdc9b2eb677652181c8951bb4ae7de9b261ac
abbask1 <aak9@illinois.edu>
2020-05-04 11:06:14 +0500

    new push

--- app/src/main/java/com/example/finalprojectcs125/virus.java
+++ app/src/main/java/com/example/finalprojectcs125/virus.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
