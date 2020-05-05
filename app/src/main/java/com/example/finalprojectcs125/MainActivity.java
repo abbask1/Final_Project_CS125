c1ad21cdd56aada549f7954c72006dc1bacfaf63
qasim <kassimand@gmail.com>
2020-05-04 22:04:28 -0500

    Pushed from K

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button)findViewById(R.id.button);
+        button = (Button)findViewById(R.id.button1);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
